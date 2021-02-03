2330b71b3b060f3389dcf98f4ebd7493a99b7c48
Anirudh-A <40145042+Anirudh-A@users.noreply.github.com>
2021-02-03 00:03:40 +0000

    string to parentheses

---
+++ string-to-parentheses/app.py
@@ -0,0 +1,28 @@
+def stringToParentheses(inputString):
+    if not(bool(inputString)):
+        print('Invalid input')
+        return
+
+    uniqueCharacters = dict()
+
+    # determine unique characters
+    for character in inputString:
+        if character in uniqueCharacters:
+            uniqueCharacters.update({character: 0})
+            continue
+        uniqueCharacters.update({character: 1})
+
+    outputString = ""
+    # buil output string
+    for character in inputString:
+        if bool(uniqueCharacters[character]):
+            outputString += ')'
+            continue
+        outputString += '('
+
+    return outputString
+
+
+inputString = input('Enter string to be converted to parentheses : ')
+outputString = stringToParentheses(inputString)
+print('After conversion: ' + outputString)
